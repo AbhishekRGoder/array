761d0ce79b373da4db603bd8f2dc2012e72537a8
AbhishekRGoder <abhishekgoder25@gmail.com>
2023-10-16 22:14:42 +0530

    string

---
+++ patterns/src/patterns/Pattern2.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j>=1;j--) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
---
+++ patterns/src/patterns/Patterns.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class Patterns {
+	public static void main(String[] args) {
+		System.out.println("Start");
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
